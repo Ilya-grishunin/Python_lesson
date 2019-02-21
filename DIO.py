bc5758f58e00fcf6b81eb4639973b3c95e442ad5
Ilya-grishunin <Igrishunin@gmail.com>
2019-02-21 23:35:35 +0300

    битва ДЫО

---
+++ DIO.py
@@ -0,0 +1,12 @@
+def battle(jotaro, dio, kol):
+    for i in range(kol):
+        print('Muda-muda Ora-ora Muda-muda Ora-ora Muda-muda Ora-ora Muda-muda Ora-ora Muda-muda Ora-ora')
+    if jotaro > dio:
+        print("Твои кабачки проиграли")
+    elif jotaro == dio:
+        print("Nani?!")
+    else:
+        print("Xo-xo!")
+
+
+
